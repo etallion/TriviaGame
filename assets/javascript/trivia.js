ac892b384afa39940b531d6546fcad8358af859f
Will Mangimelli <willwinman@gmail.com>
2019-04-07 19:00:23 -0500

    fixed timer and results

--- assets/javascript/trivia.js
+++ assets/javascript/trivia.js
@@ -17,7 +17,7 @@ var trivia = {
              {question : "What is the closest star to the Sun?",
              ansOptions : ["Promixa Centauri", "sunials", "White dwarf stars", "twinkle stars"],
              correct : 0,
-             tag : "promixa%20centauri"
+             tag : "galaxy"
              },
              {question : "What has a gravitational pull so strong that even light cannot escape it?",
              ansOptions : ["Jupiter", "Dark Matter", "A Worm Hole", "A Black Hole"],
@@ -27,126 +27,193 @@ var trivia = {
              {question : "Which NASA space flight was the last manned mission to the moon?",
              ansOptions : ["Falcon Heavy", "Apollo 2", "Apollo 17", "Sputnik 1"],
              correct : 2,
-             tag : "apollo%20moon"
+             tag : "nasa%20shuttle"
              },
              {question : "How many moons are in our Solar System?",
              ansOptions : ["1", "9", "181", "over 1200"],
              correct : 2,
              tag : "moons"
              },
-             {question : "What is the most common type of star found in the Milky Way?",
-             ansOptions : ["Red dwarf stars", "sunials", "White dwarf stars", "twinkle stars"],
+             {question : "What flavor ice cream did Baskin-Robbins release in 1969 to commemorate America’s landing on the moon?",
+             ansOptions : ["Milky Way Rocket Pop", "Rocky Caramel Moon ", "Lunar Cheesecake", "Astronaut Ice Cream"],
              correct : 0,
-             tag : "black%20hole"
+             tag : "cheesecake%20ice%20cream"
              },
-             {question : "What is the most common type of star found in the Milky Way?",
-             ansOptions : ["Red dwarf stars", "sunials", "White dwarf stars", "twinkle stars"],
+             {question : "The size of the Earth's moon is what percentage of the size of the Earth?",
+             ansOptions : ["Less than 1%", "15%", "27%", "100%, they are the same size"],
              correct : 0,
-             tag : "black%20hole"
-             }, {question : "What is the most common type of star found in the Milky Way?",
+             tag : "earth%20moon"
+             }, {question : "What ?",
              ansOptions : ["Red dwarf stars", "sunials", "White dwarf stars", "twinkle stars"],
              correct : 0,
              tag : "black%20hole"
              }],
     init : function (){
-        trivia.timer = clearTimeout;
-        trivia.optionsDiv.html('<button type="button" id="startBtn" class="btn btn-lg"> Start Game</button>');
+        clearTimeout(trivia.timerObj);
+        $("#displayBtn").html('<button type="button" id="startBtn" class="btn-lg"> Start Game</button>');
         console.log("init");
     },
     startgame : function (){
+        trivia.correct = 0
+        trivia.wrong = 0;
         console.log("starting game now");
-        $("#startBtn").addClass("hidden");
-       trivia.questionDiv.empty();
-       trivia.questionDiv.html("<h2>" + (trivia.count+1) + ". " + trivia.main[trivia.count].question);
 
-       trivia.optionsDiv.empty();
+        // start timer
+        clearInterval(trivia.intervalObj);
+        clearTimeout(trivia.timerObj);
+        trivia.interval = 10;
+        trivia.currentTime = 10;
+        trivia.intervalObj = setInterval(trivia.displayTimer, 1000);
+
+        $("#startBtn").addClass("hidden");
+       trivia.$questionDiv.empty();
+       trivia.$questionDiv.html("<h2>" + (trivia.count+1) + ". " + trivia.main[trivia.count].question);
+
+       trivia.$optionsDiv.empty();
        for(var i = 0; i < 4; i++){
-        trivia.optionsDiv.append('<div class="myOptions opt' + i + '" data-value="' + i + '">' + trivia.main[trivia.count].ansOptions[i] + "</div>");
+        trivia.$optionsDiv.append('<div class="myOptions opt' + i + '" data-value="' + i + '">' + trivia.main[trivia.count].ansOptions[i] + "</div>");
        }
        
 
     },
     nextQuestion : function (){
+        console.log("Next Question");
+        //increment counter
         trivia.count++;
+        // start timer
+        clearInterval(trivia.intervalObj);
+        clearTimeout(trivia.timerObj);
+        trivia.interval = 10;
+        trivia.currentTime = 10;
+        trivia.intervalObj = setInterval(trivia.displayTimer, 1000);
+
+
         if(trivia.count < trivia.main.length){
-            trivia.questionDiv.empty();
-            trivia.questionDiv.html("<h2>" + (trivia.count+1) + ". " + trivia.main[trivia.count].question);
+            trivia.$questionDiv.empty();
+            trivia.$questionDiv.html("<h2>" + (trivia.count+1) + ". " + trivia.main[trivia.count].question);
     
-            trivia.optionsDiv.empty();
+            trivia.$optionsDiv.empty();
             for(var i = 0; i < 4; i++){
-            trivia.optionsDiv.append('<div class="myOptions opt' + i + '" data-value="' + i + '">' + trivia.main[trivia.count].ansOptions[i] + '</div>');
+            trivia.$optionsDiv.append('<div class="myOptions opt' + i + '" data-value="' + i + '">' + trivia.main[trivia.count].ansOptions[i] + '</div>');
         }
         }else{
+            clearInterval(trivia.intervalObj);
             trivia.displayResult();
         }
 
     },
-    count : 0,
-    correct : 0,
-    wrong : 0,
     checkAnswer : function (event) {
         //start timer 
-        trivia.timer = setTimeout(trivia.nextQuestion, 5000);
+        // clearTimeout(trivia.timerObj);
+        clearInterval(trivia.intervalObj);
+   
+
 
         //hide incorrect options
         for(var i =0; i< 4; i++){
-            console.log("looping");
+            console.log("CheckAnswer Looping");
             if(trivia.main[trivia.count].correct !== i){
-                console.log('foud');
+                console.log('Found');
                 var $temp = $('.opt' + i +'');
                 $temp.addClass("hidden");
+                
             }
         }
 
-
+        // with the answer showing, change to green if they selected the correct answer, else make it red if they chose incorrectly
         if(parseInt(event) === trivia.main[trivia.count].correct){
-            
             // correct answer
-            console.log("correct"+ parseInt(event));
-            var $temp = $('.opt' + parseInt(event) +'');
+            trivia.$optionsDiv.prepend("Correct!")
+            trivia.correct++;
+            console.log("correct : "+ parseInt(event));
+            var $temp = $('.opt' + parseInt(event) + '');
             $temp.addClass("correct");
-            } else {
+        } else {
             //wrong answer
-            console.log("false"+ parseInt(event));
-            var $temp = $('.opt' + trivia.main[trivia.count].correct+'');
+            trivia.$optionsDiv.prepend("Wrong, the correct answer is :")
+            trivia.wrong++;
+            console.log("false : "+ parseInt(event));
+            var $temp = $('.opt' + trivia.main[trivia.count].correct +'');
             $temp.addClass("wrong");
-            }
+        }
 
-             //create and declare query address for a randon gif with api key
-      var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=rKTyieWDSyjDNPV9Hq5OSZY33AYum5o9&tag=" + trivia.main[trivia.count].tag;
+        //create and declare query address for a randon gif with api key
+        var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=rKTyieWDSyjDNPV9Hq5OSZY33AYum5o9&tag=" + trivia.main[trivia.count].tag + '';
         console.log(queryURL);
-      //make api call with queryURL address using a GET method
-      $.ajax({
-        url: queryURL,
-        method: "GET"
-      })
-
-      // the callback for the api call
+        //make api call with queryURL address using a GET method
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+        // the callback for the api call
         .then(function(response) {
 
         //retrieve property from the call using the response object, its data object and its image url value
-          var imageUrl = response.data.image_original_url;
+        var imageUrl = response.data.image_original_url;
 
-          //create image block
-          var image = $("<img>");
+        //create image block
+        var image = $("<img>");
 
-          //Sets source/src attribute to the image url, and sets alt text (mouse over or if image doesn't display)
-          image.attr("src", imageUrl);
-          image.attr("alt", "answer image");
+        //Sets source/src attribute to the image url, and sets alt text (mouse over or if image doesn't display)
+        image.attr("src", imageUrl);
+        image.attr("alt", "answer image");
 
-          //Adds newly created img to begin of the images div
-          trivia.optionsDiv.append(image);
+        //Adds newly created img to begin of the images div
+        trivia.$optionsDiv.append(image);
         });
-        
+
+        trivia.interval = 3;
+        trivia.currentTime = 3;
+       // trivia.timerObj = 
+        setTimeout(function(){
+            console.log("set 3 second timer");
+            trivia.nextQuestion();
+        }, (1000 * parseInt(trivia.interval)));
 
     },
     displayResult : function () {
+        trivia.$optionsDiv.empty();
+        trivia.$questionDiv.empty();
 
+        trivia.$optionsDiv.html("<div>");
+        trivia.$optionsDiv.append("Correct Answers : " + trivia.correct);
+        trivia.$optionsDiv.append("<br>Wrong Answers : " + trivia.wrong);
+
+        trivia.count = 0;
+        $("#startBtn").removeClass("hidden");
+        clearTimeout(trivia.timerObj);
     },
-    interval : null,
-    timer : null,
-    questionDiv : null,
-    optionsDiv : null
+    timer : function (){
+       
+        //Check if timer done
+        console.log("timer()" + trivia.currentTime);
+        if(trivia.currentTime > 0){
+            //still counting down
+            trivia.currentTime--;
+            console.log(trivia.currentTime);
+          //  trivia.displayTimer ();
+        } else {
+            //timer up, handle it
+            clearInterval(trivia.intervalObj);
+            //no answer, so pass null to trigger wrong answer
+            var arg = null
+            trivia.checkAnswer(arg);
+        }
+    },
+    displayTimer : function(){
+        trivia.timer();
+        $("#timerDiv").text(trivia.currentTime);
+        
+    },
+    count : 0, //Trivia question (array index) tracker.
+    correct : 0, //running total of correct answers by user.
+    wrong : 0,  //Running total of wrong answers by user.
+    interval : 0,
+    currentTime : 0,
+    timerObj : null,
+    intervalObj : null,
+    $questionDiv : null, //jQuery reference to DOM element
+    $optionsDiv : null //jQuery reference to DOM element
 
 
 }; // END OF trivia object
@@ -155,8 +222,9 @@ var trivia = {
 
 $(document).ready(function (){
 
-    trivia.questionDiv = $("#question");
-    trivia.optionsDiv = $("#options");
+    //Sets variable value to jQuery element
+    trivia.$questionDiv = $("#question");
+    trivia.$optionsDiv = $("#options");
     
     trivia.init();
 
@@ -164,8 +232,8 @@ $(document).ready(function (){
         trivia.startgame();
     });
 
-    trivia.optionsDiv.on("click", ".myOptions", function(){
+    trivia.$optionsDiv.on("click", ".myOptions", function(){
         trivia.checkAnswer($(this).attr("data-value"));
-        trivia.timer = clearTimeout;
+         clearTimeout(trivia.timerObj);
     });
 });
